eceb43f4908045ca19472d2e372f8ae764016fd8
Beingpax <prakashjoshipaxtek@gmail.com>
2025-12-12 21:24:30 +0545

    Add toggle recorder option in menu bar

--- VoiceInk/Views/MenuBarView.swift
+++ VoiceInk/Views/MenuBarView.swift
@@ -15,6 +15,12 @@ struct MenuBarView: View {
     
     var body: some View {
         VStack {
+            Button("Toggle Recorder") {
+                whisperState.handleToggleMiniRecorder()
+            }
+
+            Divider()
+
             Menu {
                 ForEach(whisperState.usableModels, id: \.id) { model in
                     Button {
@@ -182,7 +188,7 @@ struct MenuBarView: View {
             .id("additional-menu-\(menuRefreshTrigger)")
             
             Divider()
-            
+
             Button("Retry Last Transcription") {
                 LastTranscriptionService.retryLastTranscription(from: whisperState.modelContext, whisperState: whisperState)
             }
